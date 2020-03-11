4889ac3ebe27b02871a22efece0813b3b5c96356
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-03-11 15:39:21 -0500

    editblogs and homepage routers

---
+++ routes/homepage.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+let db = require("../models/database");
+
+router.get("/homepage", (req, res) => {
+  db.query("SELECT * FROM blogs").then(results => {
+    //results is an array of objects
+    res.render("homepage", {
+      blogs: results
+    });
+  });
+});
+
+module.exports = router;
